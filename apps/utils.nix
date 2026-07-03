59e69c784d4742e7d550898a52e2cfa1bdf7068f
fizzuya <fuh@pluh.bleh>
2026-07-03 21:07:31 +0200

    openrgb has startup profile and also does not crash on manual start

--- apps/utils.nix
+++ apps/utils.nix
@@ -7,7 +7,7 @@
 
 #         solaar
 #         piper
-        openrgb-with-all-plugins
+        openrgb
         logiops
         # keyd no need to mention it because services.keyd does the job for whatever reason
 
@@ -20,42 +20,44 @@
         _7zip-zstd
     ];
 
-    # stuff so mouse can be controlled
+    # adds logitech stuff stuff so mouse can be controlled
+    # like pkgs.logitech-udev-rules
     hardware.logitech.wireless.enable = true; # idk if necessary
-    services.udev.packages = with pkgs; [
-        logitech-udev-rules
-#         solaar
-    ];
+
     # for mouse rgb control
     services.hardware.openrgb = with pkgs;{
-        package = pkgs.openrgb-with-all-plugins;
         enable = true;
-    };
-
-    # make a service that controls rgb
-    systemd.user.services.g203-mouse-rgb-config = {
-        after = [ "openrgb.service" ];
-        wants = [ "openrgb.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = true;
-            ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/sleep 3 && ${pkgs.openrgb}/bin/openrgb --client 127.0.0.1 --device \"G203 LIGHTSYNC\" --mode static --color 50D5E8,FF0055,50D5E8'";
-#             ExecStart = "${pkgs.openrgb}/bin/openrgb --client 127.0.0.1 --device 'G203 LIGHTSYNC' --mode static --color 50D5E8,FF0055,50D5E8";
-#             ExecStart = "${pkgs.openrgb}/bin/openrgb --device 'G203 LIGHTSYNC' --mode static --color 50D5E8,FF0055,50D5E8";
-        };
+        startupProfile = "/home/fizzu/.config/OpenRGB/pink-cyan.orp";
     };
 
     # configure existing logiops service to use wanted dpi
-    services.logiops.config = {
+    services.logiops = {
         enable = true;
-        devices = [
-            {
-            name = "G203 LIGHTSYNC Gaming Mouse";
-            dpi = 1000;
-            }
-        ];
+        config = {
+            devices = [
+                {
+                name = "G203 LIGHTSYNC Gaming Mouse";
+                dpi = 1000;
+                }
+            ];
+        };
     };
+
+    # FUUUUUUUUUUUUUUUUUUUUUUCK THAT THING ABOVE WORKS INSTEAD OF THIS VILE DOGSHIT UUUUUUUUUGH
+    # make a service that controls rgb with more user-agency but i fucked it up somehow somewhere probably in packages
+#     systemd.services.mouse-rgb = {
+#         after = [ "openrgb.service" ];
+#         wants = [ "openrgb.service" ];
+#         wantedBy = [ "multi-user.target" ];
+#         serviceConfig = {
+#             Type = "oneshot";
+#             RemainAfterExit = true;
+#             ExecStart = [
+#             "${pkgs.coreutils}/bin/sleep 3"
+#             "${pkgs.openrgb}/bin/openrgb --client 127.0.0.1 --device \"G203 LIGHTSYNC\" --mode static --profile /home/fizzu/.config/OpenRGB/pink-cyan.orp"
+#             ];
+#         };
+#     };
 
     # NUKING the FUCK out of STUPID copilot key
     services.keyd = {
