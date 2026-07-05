87ea6fea34b2c080038fd771cb75be5d379f6706
fizzuya <fuh@pluh.bleh>
2026-07-05 13:01:26 +0200

    changed configuration.nix network manager settings a bit

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,13 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi = {
+      powersave = false;
+      macAddress = "stable-ssid";
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
