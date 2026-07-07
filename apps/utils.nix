e69d5b1cc7a572e75e729135b24397b7ac46a502
fizzuya <fuh@pluh.bleh>
2026-07-07 10:12:45 +0200

    added obs with obs-pipewire-audio-capture plugin

--- apps/utils.nix
+++ apps/utils.nix
@@ -28,7 +28,11 @@
         # thunar bc its gtk and firefox is gtk so i have an actual filepicker
         # that works well in both system and firefox portal filepicker call
         thunar
-        obs-studio
+        (wrapOBS {
+            plugins = with obs-studio-plugins;[
+                obs-pipewire-audio-capture
+            ];
+        })
     ];
 
     # adds logitech stuff stuff so mouse can be controlled
